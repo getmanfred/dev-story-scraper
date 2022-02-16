89c8da1a0adf17fadb2f5d2b019f7eff3b6849ff
Yeray Darias <ydarias@gmail.com>
2022-02-16 14:36:20 +0100

    chore: configured git hook to format code

---
+++ .lintstagedrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // Type check TypeScript files
+
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // Lint & Prettify TS and JS files
+
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint ${filenames.join(' ')}`,
+
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+};
