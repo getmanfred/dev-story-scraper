973a734ad3b4a8518139a9c1f96d4115f0432c4c
Yeray Darias <ydarias@gmail.com>
2022-02-16 16:12:02 +0100

    chore: prevent tests impacting SO all the time

--- src/tests/devStoryScraper.test.ts
+++ src/tests/devStoryScraper.test.ts
@@ -1,8 +1,8 @@
-import {DevStoryDownloader} from '../devStoryDownloader';
 import {DevStoryScraper} from '../devStoryScraper';
+import {MockDevStoryDownloader} from './mockDevStoryDownloader';
 
 describe('A Dev Story scraper', () => {
-  const downloader = new DevStoryDownloader();
+  const downloader = new MockDevStoryDownloader(); // To prevent too many requests to Stack Overflows servers
   const scraper = new DevStoryScraper(downloader);
 
   it('should parse a simple dev story', async () => {
---
+++ src/tests/mockDevStoryDownloader.ts
@@ -0,0 +1,12 @@
+import {readFileSync} from 'fs';
+
+import {DevStoryDownloader} from '../devStoryDownloader';
+
+export class MockDevStoryDownloader implements DevStoryDownloader {
+  async download(url: string): Promise<string> {
+    const user = url.split('/').reverse().shift();
+    const path = `${__dirname}/html/${user}.html`;
+
+    return readFileSync(path, 'utf8');
+  }
+}
