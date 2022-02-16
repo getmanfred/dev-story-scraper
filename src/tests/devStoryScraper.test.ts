b16cc06107be374e622a17f59ef47e6591ae2317
Yeray Darias <ydarias@gmail.com>
2022-02-16 14:12:48 +0100

    chore: added basic configuration with typescript and jest

---
+++ src/tests/devStoryScraper.test.ts
@@ -0,0 +1,9 @@
+describe('The Dev Story scraper', () => {
+    it('should parse a basic profile', () => {
+        expect(1).toBe(1);
+    });
+
+    it('should fail', () => {
+        expect(2).toBe(1);
+    });
+});
