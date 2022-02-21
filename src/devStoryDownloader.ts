defdbfe3aa568a05bebd066aa7e06f66233687c4
Yeray Darias <ydarias@gmail.com>
2022-02-21 13:34:07 +0100

    chore: added Pino as the logger system

--- src/devStoryDownloader.ts
+++ src/devStoryDownloader.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
+import {pino} from 'pino';
 
 export class DevStoryDownloader {
   async download(url: string): Promise<string> {
+    const log = pino({
+      name: 'dev-story-scraper',
+      level: 'debug',
+    });
+
+    log.info(`loading ${url}`);
+
     const response = await axios.get(url, {
       timeout: 6000,
     });
+
+    log.debug(`${url} loaded`);
+
     return response.data;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {APIGatewayProxyEvent, Handler} from 'aws-lambda';
+import {pino} from 'pino';
 
 import {DevStoryDownloader} from './devStoryDownloader';
 import {DevStoryScraper} from './devStoryScraper';
@@ -17,8 +18,16 @@ const noUsernameResponse = () => ({
 });
 
 export const handler: Handler = async (event: APIGatewayProxyEvent) => {
+  const log = pino({
+    name: 'dev-story-scraper',
+    level: 'debug',
+  });
+
   if (event.queryStringParameters && event.queryStringParameters['username']) {
     const username: string = event.queryStringParameters['username'];
+
+    log.info(`scraping information from ${username}`);
+
     const downloader = new DevStoryDownloader();
     const scraper = new DevStoryScraper(downloader);
     const profile = await scraper.parse(username);
