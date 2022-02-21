c68cddf8fc3c83ab87d571ec04ad14bff3fbc394
Yeray Darias <ydarias@gmail.com>
2022-02-21 10:38:19 +0100

    fix: connected the scraper code to the lambda handler

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-// import {DevStoryDownloader} from './devStoryDownloader';
-// import {DevStoryScraper} from './devStoryScraper';
+import {DevStoryDownloader} from './devStoryDownloader';
+import {DevStoryScraper} from './devStoryScraper';
 
 export const handler = async (): Promise<any> => {
-  // const downloader = new DevStoryDownloader();
-  // const scraper = new DevStoryScraper(downloader);
-  // const output = scraper.parse(event.username);
+  const downloader = new DevStoryDownloader();
+  const scraper = new DevStoryScraper(downloader);
+  const output = await scraper.parse('ydarias');
 
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: JSON.stringify('Hello world from dev-story-scraper'),
+    body: JSON.stringify(output),
   };
 };
