afc32afac7fe968aea0ab5c38aedc4b8457ba862
Yeray Darias <ydarias@gmail.com>
2022-02-22 08:43:55 +0100

    feat: added profiling information at debug log level

--- src/devStoryScraper.ts
+++ src/devStoryScraper.ts
@@ -6,13 +6,18 @@ import {PositionParser} from './parsers/positionParser';
 import {ArtifactParser} from './parsers/artifactParser';
 import {Markdown} from './utils/markdown';
 import {stripString} from './utils/utils';
+import {Logger} from './utils/logger';
 
 export class DevStoryScraper {
   constructor(private readonly downloader: DevStoryDownloader) {}
 
   async parse(username: string): Promise<ProfileData> {
+    const log = Logger.getInstance();
+
     const url = `https://stackoverflow.com/story/${username}`;
     const profileAsHTML = await this.downloader.download(url);
+
+    const startTime = new Date().getTime();
 
     const $ = cheerio.load(profileAsHTML);
 
@@ -52,6 +57,9 @@ export class DevStoryScraper {
         return artifactParser.parse($(e).html() || '');
       })
       .get();
+
+    const elapsed = new Date().getTime() - startTime;
+    log.debug(`${username} profile parsed in ${elapsed}ms`);
 
     return {
       name,
