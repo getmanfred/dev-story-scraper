f2d66dffbe29ce7cb428d2efdcc03ee3aaa3a30e
Yeray Darias <ydarias@gmail.com>
2022-03-02 18:00:36 +0100

    fix: only parsed the first author from a book

--- src/parsers/aboutMeParser.ts
+++ src/parsers/aboutMeParser.ts
@@ -91,14 +91,15 @@ export class DevStoryBookmarkParse {
   static parse(html: string): Bookmark {
     const $ = cheerio.load(html);
 
-    const author = stripString($('.readings-item-author').text()).replace('by ', '');
+    const authors = stripString($('.readings-item-author').text()).replace('by ', '');
+    const firstAuthor = authors.split(/\s*,\s*|\sand\s/)[0];
 
     return {
       title: stripString($('.readings-item-title').text()),
       URL: $('.readings-item-title a').attr('href'),
       author: {
-        name: _.head(author.split(' ')) || '',
-        surnames: _.tail(author.split(' ')).join(' ') || '',
+        name: _.head(firstAuthor.split(' ')) || '',
+        surnames: _.tail(firstAuthor.split(' ')).join(' ') || '',
       },
       summary: Markdown.fromHTML($('.readings-item-summary .description-content-full').html() || ''),
     };
