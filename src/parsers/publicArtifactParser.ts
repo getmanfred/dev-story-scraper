9fbbdb1acdd375b9c89d71ad6492b6a766449d33
Yeray Darias <ydarias@gmail.com>
2022-03-03 15:50:53 +0100

    fix: assessment name is taken from image title

--- src/parsers/publicArtifactParser.ts
+++ src/parsers/publicArtifactParser.ts
@@ -7,6 +7,33 @@ import * as _ from 'lodash';
 
 export class PublicArtifactParser {
   static parse(artifact: DevStoryArtifact): PublicArtifact {
+    if (artifact.type !== 'Assessment') {
+      return PublicArtifactParser.parseGenericArtifact(artifact);
+    }
+
+    return PublicArtifactParser.parseAssessmentArtifact(artifact);
+  }
+
+  private static parseAssessmentArtifact(artifact: DevStoryArtifact) {
+    const [startDate] = DatesParser.parse(artifact.time);
+
+    return {
+      details: {
+        name: artifact.logoAlt.replace(/Title:\s*/, ''),
+        description: artifact.description,
+        URL: artifact.url,
+        image: {
+          alt: artifact.logoAlt,
+          link: artifact.logo,
+        },
+      },
+      type: PublicArtifactParser.toPublicArtifactType(artifact.type),
+      publishingDate: startDate,
+      relatedCompetences: CompetenceParser.parse(artifact.tags),
+    };
+  }
+
+  private static parseGenericArtifact(artifact: DevStoryArtifact) {
     const [name] = artifact.title.split(' at ');
     const [startDate] = DatesParser.parse(artifact.time);
 
