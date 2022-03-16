73c6de9874ec728674745261934982836c0c1959
Yeray Darias <ydarias@gmail.com>
2022-03-16 16:36:26 +0100

    chore: removed unnecessary downloaders

--- src/tests/devStoryScraper.test.ts
+++ src/tests/devStoryScraper.test.ts
@@ -1,49 +1,53 @@
 import {DevStoryScraper} from '../devStoryScraper';
-import {MockDevStoryDownloader} from './mockDevStoryDownloader';
 import {MockGeocoder} from './mockGeocoder';
 import {mock} from 'jest-mock-extended';
 import {HttpClient} from '../clients/httpClient';
 import {CompanyUrlParser} from '../parsers/companyUrlParser';
 import {JobParser} from '../parsers/jobParser';
 import {readFileSync} from 'fs';
+import {DevStoryDownloader} from '../downloader/devStoryDownloader';
+import {MAC} from '../models/mac';
 
 describe('A Dev Story scraper', () => {
-  const httpClient = mock<HttpClient>();
-  const companyUrlParser = new CompanyUrlParser(httpClient);
+  const companyHttpClient = mock<HttpClient>();
+  const companyUrlParser = new CompanyUrlParser(companyHttpClient);
   const jobParser = new JobParser(companyUrlParser);
   const geocoder = new MockGeocoder();
-  const downloader = new MockDevStoryDownloader(); // To prevent too many requests to Stack Overflows servers
-  const scraper = new DevStoryScraper(downloader, geocoder, jobParser);
+  const devStoryHttpClient = mock<HttpClient>();
+  const devStoryDownloader = new DevStoryDownloader(devStoryHttpClient);
+  const scraper = new DevStoryScraper(devStoryDownloader, geocoder, jobParser);
+
+  beforeEach(() => {
+    devStoryHttpClient.getHTML.mockReset();
+
+    const html = readFileSync(`${__dirname}/html/fon.html`, 'utf8');
+    companyHttpClient.getHTML.mockResolvedValue(html);
+  });
+
+  const parseFor = async (username: string): Promise<MAC> => {
+    const html = readFileSync(`${__dirname}/html/${username}.html`, 'utf8');
+    devStoryHttpClient.getHTML.calledWith(`https://stackoverflow.com/story/${username}`).mockResolvedValue(html);
+
+    return scraper.parse(username);
+  };
 
   it('should parse the dev story from Yeray', async () => {
-    const html = readFileSync(`${__dirname}/html/fon.html`, 'utf8');
-    httpClient.getHTML.mockResolvedValue(html);
-
-    const result = await scraper.parse('ydarias');
+    const result = await parseFor('ydarias');
     expect(result).toMatchSnapshot();
   });
 
   it('should parse the dev story from Joey deVilla', async () => {
-    const html = readFileSync(`${__dirname}/html/fon.html`, 'utf8');
-    httpClient.getHTML.mockResolvedValue(html);
-
-    const result = await scraper.parse('joeydevilla');
+    const result = await parseFor('joeydevilla');
     expect(result).toMatchSnapshot();
   });
 
   it('should parse the dev story from Ferran Buireu', async () => {
-    const html = readFileSync(`${__dirname}/html/fon.html`, 'utf8');
-    httpClient.getHTML.mockResolvedValue(html);
-
-    const result = await scraper.parse('fbuireu');
+    const result = await parseFor('fbuireu');
     expect(result).toMatchSnapshot();
   });
 
   it('should parse open source projects from William Sorensen Dev Story', async () => {
-    const html = readFileSync(`${__dirname}/html/fon.html`, 'utf8');
-    httpClient.getHTML.mockResolvedValue(html);
-
-    const result = await scraper.parse('truewill');
+    const result = await parseFor('truewill');
     expect(result).toMatchSnapshot();
   });
 });
